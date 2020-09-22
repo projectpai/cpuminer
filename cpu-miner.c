cc07d7cd119995d6778e27f4315ad64a4bdcbb5a
Patrick Gerzanics <patrick.gerzanics@upandrunningsoftware.com>
2020-09-22 16:21:08 -0500

    Added paicoin algorithm doc

--- cpu-miner.c
+++ cpu-miner.c
@@ -176,6 +176,7 @@ Options:\n\
                           scrypt    scrypt(1024, 1, 1) (default)\n\
                           scrypt:N  scrypt(N, 1, 1)\n\
                           sha256d   SHA-256d\n\
+			  paicoin   SHA-256/SHAKE256 per Project PAI\n\
   -o, --url=URL         URL of mining server\n\
   -O, --userpass=U:P    username:password pair for mining server\n\
   -u, --user=USERNAME   username for mining server\n\
